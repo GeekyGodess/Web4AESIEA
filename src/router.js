b71f8ad899599fe2eaf38c2687d001482145ef87
GeekyGodess <marin.cline@gmail.com>
2019-10-28 13:29:47 +0100

    update router.js

--- src/router.js
+++ src/router.js
@@ -1,23 +1,60 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './components/Home.vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
+Vue.use(VueRouter)
 
-Vue.use(Router)
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
 
-export default new Router({
-    mode:'history',
-    base: process.env.BASE_URL,
-    routes:[
-    {
-        path:'/',
-        name : 'home',
-        component: Home
-    },
-    {
-        path:'/login',
-        name :'login',
-        component:() => import('./components/Login.vue')
-    }
+  {
+    path: '/baguettes',
+    name: 'baguettes',
+    component: () => import( '../views/Baguettes.vue')
+  },
+  {
+    path: '/bijoux',
+    name: 'bijoux',
+    component: () => import( '../views/Bijoux.vue')
+  },
+  {
+    path: '/heart',
+    name: 'heart',
+    component: () => import( '../views/Heart.vue')
+  },
+  {
+    path: '/vetements',
+    name: 'vetements',
+    component: () => import( '../views/Vetements.vue')
+  },
+  {
+    path: '/new',
+    name: 'new',
+    component: () => import( '../views/Nouveautes.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import( '../views/Login.vue')
+  }
 ]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
 })
+
+export default router
